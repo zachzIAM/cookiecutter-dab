4f581b398fa8bea021f33bdaaf697610eb166c8f
Zach Zankov <zach.zankov@ninetyone.com>
2024-02-28 09:09:40 +0000

    encapsulate all logic of local extension in the calss

--- local_extensions.py
+++ local_extensions.py
@@ -3,26 +3,18 @@ from configparser import ConfigParser
 from pathlib import Path
 from jinja2.ext import Extension
 
-# @simple_filter
-# def default_databricks_host() -> Optional[str]:
-#     config = ConfigParser()
-#     config.read(find_databrickscfg())
-#     return config.defaults().get("host")
 
 class DatabricksHostExtension(Extension):
     def __init__(self, environment):
         super(DatabricksHostExtension, self).__init__(environment)
+
+        def default_databricks_host() -> Optional[str]:
+            config = ConfigParser()
+            config.read(find_databrickscfg())
+            return config.defaults().get("host")
+
+        def find_databrickscfg() -> str:
+            path = Path().home() / ".databrickscfg"
+            return path.as_posix()
+
         environment.filters['default_databricks_host'] = default_databricks_host
-
-
-def default_databricks_host() -> Optional[str]:
-    config = ConfigParser()
-    config.read(find_databrickscfg())
-    return config.defaults().get("host")
-
-
-def find_databrickscfg() -> str:
-    path = Path().home() / ".databrickscfg"
-    return path.as_posix()
-
-
