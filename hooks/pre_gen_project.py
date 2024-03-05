8fc2d9c0759b5bce678517ca1c8d88912f1b8e2e
Zach Zankov <46534510+zachzIAM@users.noreply.github.com>
2024-03-05 18:02:53 +0000

    add pre-commit hooks and dev requirement; resolves #4 (#9)

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -6,9 +6,12 @@ def is_snake_case(value: str) -> bool:
     snake_case_pattern = "[a-z]+(?:_{1}[a-z]+)*[^-_]$"
     return bool(re.match(snake_case_pattern, value))
 
+
 package_name = "{{ cookiecutter.package_name }}"
 
 if not is_snake_case(package_name):
-    print(f"ERROR: {package_name} is not a valid package name. Make sure you are using snakecase notation")
+    print(
+        f"ERROR: {package_name} is not a valid package name. Make sure you are using snakecase notation"
+    )
 
     sys.exit(1)
