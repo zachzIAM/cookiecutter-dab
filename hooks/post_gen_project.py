28d08e2f9deb0bfd78d942cb1282eb5ecb714cd2
Kulraj Suri <kulraj.suri@ninetyone.com>
2024-03-14 12:27:51 +0000

    🚧 wip: added .github folder to cookiecutter

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 
 def remove_open_source_files():
@@ -7,9 +8,25 @@ def remove_open_source_files():
         os.remove(file_name)
 
 
+def delete_docs_folder_if_requested():
+    generate_docs = "{{ cookiecutter.automated_sphinx_docs }}".lower()
+    docs_folder = "docs"
+
+    if generate_docs == "n":
+        if os.path.exists(docs_folder):
+            shutil.rmtree(docs_folder)
+            print("Deleted /docs folder based on user choice.")
+        else:
+            print("/docs folder not found. No action required.")
+    else:
+        print("Retaining /docs folder based on user choice.")
+
+
 def main():
     if "{{ cookiecutter.open_source_license }}" == "Not open source":
         remove_open_source_files()
+
+    delete_docs_folder_if_requested()
 
 
 if __name__ == "__main__":
---
+++ {{cookiecutter.project_slug}}/.github/ISSUE_TEMPLATE/BUG_REPORT.md
@@ -0,0 +1,32 @@
+---
+name: "🐞 Bug Report"
+about: Create a report to help us improve
+title: ""
+labels: ["bug"]
+assignees: ""
+---
+
+<!--
+Found a bug? Please fill out the sections below. 👍
+-->
+
+### Issue Summary
+
+<!--
+A summary of the issue.
+-->
+
+### Steps to Reproduce
+
+1. step 1
+2. step 2
+3. ...
+
+Any other relevant information. For example, why do you consider this a bug and what did you expect to happen instead?
+
+- I have confirmed that this issue can be reproduced as described on a fresh install of the project: (yes / no)
+
+### Technical details
+
+- Python version: Run `python --version`.
+- Project package version: Run `pip show {{ cookiecutter.package_name }} | grep Version:`.
